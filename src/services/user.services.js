180218417616f7d22a509f1d9fb236926b317417
Kevin Sencherey <ksscholastic@gmail.com>
2022-12-09 19:42:23 +0000

    user validation added

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,12 +1,31 @@
 const db = require("../config/database");
+const bcrypt = require("bcrypt");
 
 //create a new user
 const createUser = async (body) => {
   const { first_name, last_name, email, password, confirm_password } = body;
+
+  //check if user already exists
+  const checkUser = await db.query("SELECT * FROM 'users' WHERE email = $1", [
+    email,
+  ]);
+
+  if (checkUser.rows.length > 0) {
+    throw new Error("User already exists");
+  }
+
+  //check if passwords match
+  if (password !== confirm_password) {
+    throw new Error("Passwords do not match");
+  }
+
+  //hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   //in database.js, remember db accepts  text and params
   const { rows } = await db.query(
     "INSERT INTO users (first_name, last_name, email, password) VALUES($1, $2, $3, $4) RETURNING *",
-    [first_name, last_name, email, password]
+    [first_name, last_name, email, hashedPassword]
   );
   return rows[0];
 };
